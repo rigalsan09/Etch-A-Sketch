72ad0f76b939ad969aa08f699bb186a64e7777cd
rigalsan09 <rigallegos09@gmail.com>
2023-03-19 20:26:27 -0700

    add existing file

---
+++ script.js
@@ -0,0 +1,13 @@
+//DOM Manipulation
+
+// GetElementById()
+const title = document.getElementById("main-heading");
+
+alert(title);
+// GetElementByClassName()
+
+// getElementsByTagName()
+
+// querySelector()
+
+// querySelectorAll()
